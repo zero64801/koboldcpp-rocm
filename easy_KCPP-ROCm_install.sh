78636109fc2ded79ee3e9a44d2e3c2d63a8de70e
YellowRoseCx <80486540+YellowRoseCx@users.noreply.github.com>
2023-07-26 13:27:22 -0500

    Update easy_KCPP-ROCm_install.sh

--- easy_KCPP-ROCm_install.sh
+++ easy_KCPP-ROCm_install.sh
@@ -1,7 +1,19 @@
+# #!/bin/bash
+
+# git clone https://github.com/YellowRoseCx/koboldcpp-rocm.git -b main --depth 1 && \
+# cd koboldcpp-rocm && \
+# make LLAMA_HIPBLAS=1 -j4 && \
+# ./koboldcpp.py
+
 #!/bin/bash
 
-git clone https://github.com/YellowRoseCx/koboldcpp-rocm.git -b main --depth 1 && \
-cd koboldcpp-rocm && \
+if [ "$(basename "$PWD")" = "koboldcpp-rocm" ]; then
+  echo "Already inside 'koboldcpp-rocm' directory."
+else
+  git clone https://github.com/YellowRoseCx/koboldcpp-rocm.git -b main --depth 1 && \
+  cd "koboldcpp-rocm" || exit 1
+fi
+
+
 make LLAMA_HIPBLAS=1 -j4 && \
 ./koboldcpp.py
-
