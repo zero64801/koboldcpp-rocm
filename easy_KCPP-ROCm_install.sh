b650b849d52aac65364558521f76e75ded7ea590
YellowRoseCx <80486540+YellowRoseCx@users.noreply.github.com>
2023-07-30 00:21:36 -0500

    Update easy_KCPP-ROCm_install.sh

--- easy_KCPP-ROCm_install.sh
+++ easy_KCPP-ROCm_install.sh
@@ -1,11 +1,13 @@
-# #!/bin/bash
-
-# git clone https://github.com/YellowRoseCx/koboldcpp-rocm.git -b main --depth 1 && \
-# cd koboldcpp-rocm && \
-# make LLAMA_HIPBLAS=1 -j4 && \
-# ./koboldcpp.py
-
 #!/bin/bash
+function countdown {
+  local num=$1
+  while [ $num -gt 0 ]; do
+    printf "\rAbout to build KoboldCPP-ROCm in %s seconds..." $num
+    sleep 1
+    num=$((num - 1))
+  done
+  printf "\Building KoboldCPP...   \n"
+}
 
 if [ "$(basename "$PWD")" = "koboldcpp-rocm" ]; then
   echo "Already inside 'koboldcpp-rocm' directory."
@@ -14,6 +16,9 @@ else
   cd "koboldcpp-rocm" || exit 1
 fi
 
+echo "Build will start shortly."
+countdown 5
 
-make LLAMA_HIPBLAS=1 -j4 && \
+make clean && \
+make LLAMA_HIPBLAS=1 LLAMA_OPENBLAS=1 LLAMA_CLBLAST=1 -j4 && \
 ./koboldcpp.py
