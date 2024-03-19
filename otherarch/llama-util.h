942fb4b413339b5045a0e699e12469ea4b7b3f92
Concedo <39025047+LostRuins@users.noreply.github.com>
2024-03-19 19:26:45 +0800

    fixed removed ref (+1 squashed commits)
    Squashed commits:
    
    [93f3c270] fixed removed ref (+1 squashed commits)
    
    Squashed commits:
    
    [df361250] remove some files

--- otherarch/llama-util.h
+++ otherarch/llama-util.h
@@ -505,7 +505,7 @@ struct llama_v3_buffer {
 };
 
 #ifdef GGML_USE_CUBLAS
-#include "ggml-cuda.h"
+#include "ggml_v3-cuda.h"
 struct llama_v3_ctx_buffer {
     uint8_t * addr = NULL;
     bool is_cuda;
@@ -516,7 +516,7 @@ struct llama_v3_ctx_buffer {
     void resize(size_t size) {
         free();
 
-        addr = (uint8_t *) ggml_cuda_host_malloc(size);
+        addr = (uint8_t *) ggml_v3_cuda_host_malloc(size);
         if (addr) {
             is_cuda = true;
         }
@@ -531,7 +531,7 @@ struct llama_v3_ctx_buffer {
     void free() {
         if (addr) {
             if (is_cuda) {
-                ggml_cuda_host_free(addr);
+                ggml_v3_cuda_host_free(addr);
             }
             else {
                 delete[] addr;
