7a5983399bad686b73eaf3464a47bc0ae88eceb2
Concedo <39025047+LostRuins@users.noreply.github.com>
2024-05-15 23:49:01 +0800

    try to fix lora naming issues

--- otherarch/sdcpp/ggml_extend.hpp
+++ otherarch/sdcpp/ggml_extend.hpp
@@ -701,8 +701,8 @@ __STATIC_INLINE__ struct ggml_tensor* ggml_nn_timestep_embedding(
 //     virtual struct ggml_cgraph* get_ggml_cgraph() = 0;
 // };
 
-#define MAX_PARAMS_TENSOR_NUM 10240
-#define MAX_GRAPH_SIZE 10240
+#define MAX_PARAMS_TENSOR_NUM 15360
+#define MAX_GRAPH_SIZE 15360
 
 struct GGMLModule {
 protected:
