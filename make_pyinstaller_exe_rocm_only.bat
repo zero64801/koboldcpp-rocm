d2f5e1f19f321a8ed6116e32660f773e354a3d75
YellowRoseCx <80486540+YellowRoseCx@users.noreply.github.com>
2024-05-26 14:12:49 -0500

    use tar not 7z in make_pyinstaller_exe_rocm_only.bat

--- make_pyinstaller_exe_rocm_only.bat
+++ make_pyinstaller_exe_rocm_only.bat
@@ -3,7 +3,7 @@ copy "C:\Program Files\AMD\ROCm\5.7\bin\hipblas.dll" .\ /Y
 copy "C:\Program Files\AMD\ROCm\5.7\bin\rocblas.dll" .\ /Y
 xcopy /E /I "C:\Program Files\AMD\ROCm\5.7\bin\rocblas" .\rocblas\ /Y
 curl -LO https://github.com/YellowRoseCx/koboldcpp-rocm/releases/download/v1.43.2-ROCm/gfx103132rocblasfiles.zip
-7z x gfx103132rocblasfiles.zip
+tar -xf gfx103132rocblasfiles.zip -C .\ --strip-components=1
 pip install customtkinter
  
 PyInstaller --noconfirm --onefile --clean --console --collect-all customtkinter --collect-all psutil --icon "./niko.ico" --add-data "./winclinfo.exe;." --add-data "./klite.embd;." --add-data "./kcpp_docs.embd;." --add-data="./kcpp_sdui.embd;." --add-data="./taesd.embd;." --add-data="./taesd_xl.embd;." --add-data "./rwkv_vocab.embd;." --add-data "./rwkv_world_vocab.embd;." --add-data "./koboldcpp_hipblas.dll;." --add-data "C:/Program Files/AMD/ROCm/5.7/bin/hipblas.dll;." --add-data "C:/Program Files/AMD/ROCm/5.7/bin/rocblas.dll;." --add-data "C:/Program Files/AMD/ROCm/5.7/bin/rocblas;." --add-data "C:/Windows/System32/msvcp140.dll;." --add-data "C:/Windows/System32/vcruntime140_1.dll;." "./koboldcpp.py" -n "koboldcpp_rocm.exe"
