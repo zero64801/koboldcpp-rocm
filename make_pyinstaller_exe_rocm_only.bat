7636580b887de991254cf52432a327ad3baae5aa
YellowRoseCx <80486540+YellowRoseCx@users.noreply.github.com>
2024-05-26 13:49:04 -0500

    Update make_pyinstaller_exe_rocm_only.bat

--- make_pyinstaller_exe_rocm_only.bat
+++ make_pyinstaller_exe_rocm_only.bat
@@ -1,7 +1,9 @@
 cd /d "%~dp0"
-copy "C:\Program Files\AMD\ROCm\5.5\bin\hipblas.dll" .\ /Y
-copy "C:\Program Files\AMD\ROCm\5.5\bin\rocblas.dll" .\ /Y
-xcopy /E /I "C:\Program Files\AMD\ROCm\5.5\bin\rocblas" .\rocblas\ /Y
+copy "C:\Program Files\AMD\ROCm\5.7\bin\hipblas.dll" .\ /Y
+copy "C:\Program Files\AMD\ROCm\5.7\bin\rocblas.dll" .\ /Y
+xcopy /E /I "C:\Program Files\AMD\ROCm\5.7\bin\rocblas" .\rocblas\ /Y
+curl -LO https://github.com/YellowRoseCx/koboldcpp-rocm/releases/download/v1.43.2-ROCm/gfx103132rocblasfiles.zip
+7z x gfx103132rocblasfiles.zip
 pip install customtkinter
  
-PyInstaller --noconfirm --onefile --collect-all customtkinter --clean --console --icon ".\niko.ico" --add-data "./klite.embd;." --add-data "./kcpp_docs.embd;." --add-data "./koboldcpp_hipblas.dll;." --add-data "./hipblas.dll;." --add-data "./rocblas.dll;." --add-data "./rwkv_vocab.embd;." --add-data "./rwkv_world_vocab.embd;." --add-data "./rocblas;." --add-data "C:/Windows/System32/msvcp140.dll;." --add-data "C:/Windows/System32/vcruntime140_1.dll;." "./koboldcpp.py" -n "koboldcpp_rocm_only.exe"
+PyInstaller --noconfirm --onefile --clean --console --collect-all customtkinter --collect-all psutil --icon "./niko.ico" --add-data "./winclinfo.exe;." --add-data "./klite.embd;." --add-data "./kcpp_docs.embd;." --add-data="./kcpp_sdui.embd;." --add-data="./taesd.embd;." --add-data="./taesd_xl.embd;." --add-data "./rwkv_vocab.embd;." --add-data "./rwkv_world_vocab.embd;." --add-data "./koboldcpp_hipblas.dll;." --add-data "C:/Program Files/AMD/ROCm/5.7/bin/hipblas.dll;." --add-data "C:/Program Files/AMD/ROCm/5.7/bin/rocblas.dll;." --add-data "C:/Program Files/AMD/ROCm/5.7/bin/rocblas;." --add-data "C:/Windows/System32/msvcp140.dll;." --add-data "C:/Windows/System32/vcruntime140_1.dll;." "./koboldcpp.py" -n "koboldcpp_rocm.exe"
