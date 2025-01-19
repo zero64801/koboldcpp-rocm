bf4a52383f532868b10988fcfcc87ff28165c585
Concedo <39025047+LostRuins@users.noreply.github.com>
2025-01-19 22:53:38 +0800

    change of plans, we can't bundle numpy

--- make_pyinstaller.sh
+++ make_pyinstaller.sh
@@ -3,7 +3,6 @@ chmod +x "./create_ver_file.sh"
 . create_ver_file.sh
 pyinstaller --noconfirm --onefile --clean --console --collect-all customtkinter --collect-all psutil --icon "./niko.ico" \
 --add-data "./kcpp_adapters:./kcpp_adapters" \
---add-data "./gguf-py:./gguf-py" \
 --add-data "./koboldcpp.py:." \
 --add-data "./klite.embd:." \
 --add-data "./kcpp_docs.embd:." \
