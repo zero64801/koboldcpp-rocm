35dc8387e94062537df9a05ea795a8a75b557e0d
Concedo <39025047+LostRuins@users.noreply.github.com>
2025-04-26 02:13:06 +0800

    fixed rwkv7 handling

--- model_adapter.cpp
+++ model_adapter.cpp
@@ -325,7 +325,7 @@ void print_tok_vec(std::vector<float> &embd)
             {
                 fileformatmeta->model_architecture = GGUFArch::ARCH_GEMMA3;
             }
-            else if(modelarch=="rwkv6")
+            else if(modelarch=="rwkv6" || modelarch=="rwkv7")
             {
                 fileformatmeta->model_architecture = GGUFArch::ARCH_RWKV;
             }
